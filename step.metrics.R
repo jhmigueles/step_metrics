72285f5789e6e05ed56bf4f6163408fb549fe9dc
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2020-06-26 11:13:41 +0200

    Update step.metrics.R

--- step.metrics.R
+++ step.metrics.R
@@ -9,7 +9,7 @@ step.metrics = function(datadir, outputdir="./",
   
   # Functions
   chartime2iso8601 = function(x,tz = ""){
-    POStime = as.POSIXlt(as.numeric(as.POSIXlt(x,tz),origin="1970-01-01",tz)
+    POStime = as.POSIXlt(as.numeric(as.POSIXlt(x,tz)),origin="1970-01-01",tz)
     POStimeISO = strftime(POStime,format="%Y-%m-%dT%H:%M:%S%z")
     return(POStimeISO)
   }
